93943870f5e8b16e51790b9e3578f4298d145c7b
VadimTalimonchik <tvadmins@gmail.com>
2022-12-06 12:59:52 +0300

    Задача 20 (исправил решение)

--- task_20/Program.cs
+++ task_20/Program.cs
@@ -5,4 +5,16 @@
 // A (3,6); B (2,1) -> 5,09 
 // A (7,-5); B (1,-1) -> 7,21
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите координату XA: ");
+int XA = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите координату YA: ");
+int YA = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите координату XB: ");
+int XB = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите координату YB: ");
+int YB = int.Parse(Console.ReadLine()!);
+
+double pif = Math.Sqrt((XB - XA)*(XB - XA) + (YB - YA)*(YB - YA));
+double result = pif;
+Console.WriteLine(result);
